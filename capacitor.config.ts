b1bb5c515eb2f74d6bf276769a4dadfd8a9a4f39
Juryn Tamtam <juryntamtam@gmail.com>
2024-05-08 10:20:25 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.ws32',
+  appName: 'ws32',
+  webDir: 'www'
+};
+
+export default config;
